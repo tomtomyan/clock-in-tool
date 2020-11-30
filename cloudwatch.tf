288c2c22bcce51f935c7541fd648f8941e24c3d4
Tom Yan <tomtomtomtomyan@gmail.com>
2020-11-29 21:12:35 -0500

    enable cloudwatch

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,14 +1,14 @@
 resource "aws_cloudwatch_event_rule" "clock_in" {
   name                = "Clock_In"
   description         = "Clocks in at 9:00 on weekdays"
-  is_enabled          = false
+  is_enabled          = true
   schedule_expression = "cron(0 13 ? * MON-FRI *)"
 }
 
 resource "aws_cloudwatch_event_rule" "clock_out" {
   name                = "Clock_Out"
   description         = "Clocks out at 5:30 on weekdays"
-  is_enabled          = false
+  is_enabled          = true
   schedule_expression = "cron(30 21 ? * MON-FRI *)"
 }
 
